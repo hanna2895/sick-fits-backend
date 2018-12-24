d90311110ae395e60fd6b5deb6667f0c8c779689
Hannah Werman <hanna.w28@gmail.com>
2018-12-24 10:25:54 -0600

    updating items with queries and mutations

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,12 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
     items: forwardTo('db'),
-    // this is replaced by the above: 
-    // async items(parent, args, ctx, info) {
-    //     const items = await ctx.db.query.items();
-    //     return items;
-    // }
-
+    item: forwardTo('db'),
 };
 
 module.exports = Query;
