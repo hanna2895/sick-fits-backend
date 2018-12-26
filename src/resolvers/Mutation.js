96ebd2c77e5fb5edcb12a1e30b0697157286f1ec
Hannah Werman <hanna.w28@gmail.com>
2018-12-26 10:51:24 -0600

    delete item mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -22,6 +22,15 @@ const Mutations = {
                 id: args.id
             }
         }, info );
+    },
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // find the item 
+        const item = await ctx.db.query.item({ where }, `{ id title }`)
+        // check if they own that item or have the permissions
+        // TODO 
+        // delete it
+        return ctx.db.mutation.deleteItem({ where }, info);
     }
 };
 
