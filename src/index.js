96b5aaa091fc53855fff53bfcfb68e95886ef3ca
Hannah Werman <hanna.w28@gmail.com>
2019-02-08 12:12:19 -0600

    permissions management

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const server = createServer();
 // use express middleware to handle cookies (JWT)
 server.express.use(cookieParser());
 
+// decode the JWT so we can get the user id on each request
 server.express.use((req, res, next) => {
     const { token } = req.cookies;
     if (token) {
@@ -19,6 +20,18 @@ server.express.use((req, res, next) => {
     }
     next();
   });
+
+// create a middleware that populates the user on each request
+server.express.use(async (req, res, next) => {
+    // if they aren't logged in, skip this
+    if (!req.userId) return next();
+    const user = await db.query.user(
+        { where: {id: req.userId } },
+        '{ id, permissions, email, name}'
+    );
+    req.user = user;
+    next();
+})
 
 server.start({
     cors: {
