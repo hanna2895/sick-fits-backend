be4ce61bcfcfb6a86a25abc697b3aa9b59f05671
Hannah Werman <hanna.w28@gmail.com>
2019-02-17 10:19:03 -0600

    removing cart items

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -230,6 +230,24 @@ const Mutations = {
                 }
             }
         }, info)
+    },
+    async removeFromCart(parent, args, ctx, info) {
+        // 1. find the cart item
+        const cartItem = await ctx.db.query.cartItem({
+            where: {
+                id: args.id,
+            }
+        }, `{id, user {id }}`)
+        // 1.5 make sure we found an item
+        if (!cartItem) throw new Error('No CartItem Found');
+        // 2. make sure they own that cart item
+        if (cartItem.user.id !== ctx.request.userId) {
+            throw new Error ('cheating huuuhhh')
+        }
+        // 3. delete that cart item
+        return ctx.db.mutation.deleteCartItem({
+            where: { id: args.id },
+        }, info);
     }
 };
 
