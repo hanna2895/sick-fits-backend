ef24330cc8731fff292219c1d388f4a20400f3e5
Hannah Werman <hanna.w28@gmail.com>
2019-03-03 14:00:31 +0700

    saving orders to the database

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -251,41 +251,63 @@ const Mutations = {
         }, info);
     },
     async createOrder(parent, args, ctx, info) {
-        // 1. query the current user and make sure they are signed in
+        // 1. Query the current user and make sure they are signed in
         const { userId } = ctx.request;
-        if(!userId) throw new Error('You must be signed in to complete this order.');
-        const user = await ctx.db.query.user({where: { id: userId }}, 
-            `{ 
-                id 
-                name 
-                email 
-                cart { 
-                    id 
-                    quantity 
-                    item { 
-                        title 
-                        price 
-                        id 
-                        description 
-                        image
-                    }
-                }}`
-            );
+        if (!userId) throw new Error('You must be signed in to complete this order.');
+        const user = await ctx.db.query.user(
+          { where: { id: userId } },
+          `{
+          id
+          name
+          email
+          cart {
+            id
+            quantity
+            item { title price id description image largeImage }
+          }}`
+        );
         // 2. recalculate the total for the price
-        const amount = user.cart.reduce((tally, cartItem) => tally + cartItem.item.price * cartItem.quantity, 0 );
-        console.log(`Going to charge for a total of ${amount}`)
-        console.log(process.env.STRIPE_SECRET)
-        // 3. create the stripe charge (turn token into money)
+        const amount = user.cart.reduce(
+          (tally, cartItem) => tally + cartItem.item.price * cartItem.quantity,
+          0
+        );
+        console.log(`Going to charge for a total of ${amount}`);
+        // 3. Create the stripe charge (turn token into $$$)
         const charge = await stripe.charges.create({
-            amount,
-            currency: 'USD',
-            source: args.token,
-        })
-        // 4. convert the cart items to order items 
-        // 5. create the order 
-        // 6. clear the user's cart. delete cartItems
-        // 7. return the order to the client
-    }
+          amount,
+          currency: 'USD',
+          source: args.token,
+        });
+        // 4. Convert the CartItems to OrderItems
+        const orderItems = user.cart.map(cartItem => {
+          const orderItem = {
+            ...cartItem.item,
+            quantity: cartItem.quantity,
+            user: { connect: { id: userId } },
+          };
+          delete orderItem.id;
+          return orderItem;
+        });
+    
+        // 5. create the Order
+        const order = await ctx.db.mutation.createOrder({
+          data: {
+            total: charge.amount,
+            charge: charge.id,
+            items: { create: orderItems },
+            user: { connect: { id: userId } },
+          },
+        });
+        // 6. Clean up - clear the users cart, delete cartItems
+        const cartItemIds = user.cart.map(cartItem => cartItem.id);
+        await ctx.db.mutation.deleteManyCartItems({
+          where: {
+            id_in: cartItemIds,
+          },
+        });
+        // 7. Return the Order to the client
+        return order;
+      },
 };
 
 module.exports = Mutations;
